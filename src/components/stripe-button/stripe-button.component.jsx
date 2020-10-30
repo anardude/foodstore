a4300ae058dd34884d584e3a8a6e17ad71de8d34
Manu <manu035@gmail.com>
2020-10-30 19:12:47 +0100

    added stripe button

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+    const priceForStripe = price * 100;
+    const publishableKey = 'pk_test_51HhsmUJLHzP5XVEXui857FVI22nACTuMGU2wHrKs2z91UwLrqQfrvmN5XKnV7PIJbCcXlJOBtYDj5tJy1rgeXHF200SYG5glMh';
+
+    const onToken = token => {
+        console.log(token);
+        alert('Payment Succesful');
+    }
+
+    return (
+        <StripeCheckout
+            label='Pay Now'
+            name='Food Store'
+            billingAdress
+            shippingAddress
+            image='https://svgshare.com/i/CUz.svg'
+            description={`Your total is ${price}€`}
+            amount={priceForStripe}
+            panelLabel='Pay Now'
+            token={onToken}
+            stripeKey={publishableKey}
+        />
+    );
+};
+
+export default StripeCheckoutButton;
